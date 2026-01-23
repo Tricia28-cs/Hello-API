14cbaff42d647b29a1b8c2b223ae04a01dd7ec3d
Lwin Moh Moh Theint <u6722112@au.edu>
2026-01-23 13:53:21 +0700

    Create Hello API

---
+++ src/app/api/hello/route.js
@@ -0,0 +1,10 @@
+import { NextResponse } from "next/server";
+
+export async function GET() {
+    
+  const message = {
+    message: "hello world",
+  };
+
+  return NextResponse.json(message);
+}
